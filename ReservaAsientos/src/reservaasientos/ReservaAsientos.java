26e5b0c942738c45862e5674a31e29ed46ee88e1
Cristian Castillo <crcastillo@ledesma.com.ar>
2023-10-12 00:40:35 -0300

    Fin del cursado.

--- ReservaAsientos/src/reservaasientos/ReservaAsientos.java
+++ ReservaAsientos/src/reservaasientos/ReservaAsientos.java
@@ -10,19 +10,23 @@ public class ReservaAsientos {
         char asientos[][] = new char[10][10];
         boolean bandera = false;
         Scanner leer = new Scanner(System.in);
-        int fila, asiento;
+        int fila=0, asiento=0;
         String respuesta;
         String verMapa;
-
+        
+        //cargamos la matriz de asientos
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 asientos[i][j] = 'L';
             }
         }
 
+        //bienvenida al sistema
         System.out.println("-------BIENVENIDO AL SISTEMA DE RESERVAS-------");
 
+        //reserva de asientos
         while (bandera != true) {
+            //visualización de mapa
             System.out.println("Desea ver los asientos disponibles? S: Si, cualquier otra letra: No");
             //acá vamos a llamar al método.
             verMapa = leer.next();
@@ -31,13 +35,27 @@ public class ReservaAsientos {
             dibujarMapa(asientos);    
             }
             
-
+            //reserva
+            boolean estaOk = false;
+            while (estaOk!=true) {
+                
             System.out.println("Ingrese Fila y Asiento a reservar");
             System.out.print("Fila (entre 0 y 9): ");
             fila = leer.nextInt();
 
             System.out.print("Asiento (entre 0 y 9): ");
             asiento = leer.nextInt();
+            
+                if (fila<=9 && fila>=0) {
+                    if (asiento<=9 && asiento>=0) {
+                        estaOk = true;
+                    } else {
+                        System.out.println("El número de asiento no es válido.");
+                    }
+                } else {
+                    System.out.println("El número de fila no es válido.");
+                }
+            }
 
             if (asientos[fila][asiento] == 'L') {
                 asientos[fila][asiento] = 'X';
